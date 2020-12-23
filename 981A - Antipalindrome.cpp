d793fe240fda7673d2310b25c38a4318f36d1b69
shubham1708 <61815446+shubham1708@users.noreply.github.com>
2020-12-23 11:38:39 +0530

    Create 981A - Antipalindrome.cpp

---
+++ 981A - Antipalindrome.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+bool ispalindron(string s){
+	string y=s;
+	reverse(s.begin(),s.end());
+	return y==s;
+}
+ 
+ 
+ 
+ 
+int main() {
+	string s;
+	cin>>s;
+	int len=s.length();
+	int result=0;
+	for(int i=0;i<=len;i++){
+		for(int j=0;j<=len;j++){
+			string a=s.substr(i,j);
+			if(!ispalindron(a)){
+				int l=a.length();
+				result=max(result,l);
+			}
+		}
+		
+		
+	}
+	cout<<result;
+	
+	return 0;
+}
