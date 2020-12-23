029c521e9d2cc63298d48c6be07b79db5d1aa484
shubham1708 <61815446+shubham1708@users.noreply.github.com>
2020-12-23 11:53:36 +0530

    Create 513A - Game.cpp

---
+++ 513A - Game.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n1,n2,k1,k2;  cin>>n1>>n2>>k1>>k2;
+	int count=0,count2=0;
+	if(n1>n2){
+	    cout<<"First"<<endl;
+	}else if(n2>=n1){
+	    cout<<"Second"<<endl;
+	}
+	
+}
