980f93a7412b97bade00953ee488ec723f261cd5
shubham1708 <61815446+shubham1708@users.noreply.github.com>
2020-12-30 07:29:52 +0530

    Create 879A - Borya's Diagnosis.cpp

---
+++ 879A - Borya's Diagnosis.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main(){
+    int n;  cin>>n;
+    int a,b,max=0,count=0;
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        while(a<=max){
+          a+=b;
+        }max=a;
+    }cout<<a;
+   
+}
