2471e9ff05a77fba9a5f72a171a233dde893cc99
shubham1708 <61815446+shubham1708@users.noreply.github.com>
2022-04-17 09:46:33 +0530

    Create A. Vasya and Cnadies.cpp
    Codeforces Round #780 (Div. 3)
    Shubham yadav 
    Computer Science Engineering
    Manav Rachna University

---
+++ A. Vasya and Cnadies.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        if(a>0){
+            cout<<b*2+a+1<<endl;
+        }else{
+            cout<<1<<endl;
+        }
+    }
+    }
+
+
+   
